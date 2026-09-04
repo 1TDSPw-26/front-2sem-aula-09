87fe009da3bd77aa2a6dfac24d11e9871e5bee86
Ludmylla1905 <ludmilanogueira231@gmail.com>
2026-09-03 22:03:05 -0300

    feat: Modularizando o App com cabecalho, conteudo e rodape.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,14 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Cabecalho/>
+      <Conteudo/>
+      <Rodape/>
+    </div>
   )
 }
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,10 @@
+export default function Conteudo(){
+    return(
+        <main>
+            <section>
+                <h2>Conteúdo</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae, inventore expedita nulla adipisci debitis nemo, quibusdam voluptas, nostrum dicta tempore officia illum temporibus error totam consectetur suscipit dolorem odit?</p>
+            </section>
+        </main>
+    );
+}
