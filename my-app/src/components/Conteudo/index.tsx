7f4c81ca06f5be999396aa6ee6f253762fa97a23
NickRM22 <nick.rodrigues2005@gmail.com>
2026-09-03 22:50:21 -0300

    feat: trabalhando com imagens

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,8 +1,26 @@
+import imgQuadrado from "../../img/quadrado.png";
+
 export default function Conteudo(){
     return(
         <main>
             <section>
                 <h2>Conteúdo</h2>
+
+                <figure>
+                    <img src="https://placehold.co/600x400/FFFFF0/FFFFFF/png" alt="figura de demonstração" />
+                    <figcaption>Imagem externa</figcaption>
+                </figure>
+
+                <figure>
+                    <img src= {imgQuadrado} alt="figura de demonstração" />
+                    <figcaption>Imagem interna</figcaption>
+                </figure>
+
+                <figure>
+                    <img src="/quadrado-b.png" alt="figura de demonstração" />
+                    <figcaption>Imagem estatico</figcaption>
+                </figure>
+
                 <p>Lorem ipsum dolor sit amet.</p>
             </section>
         </main>
