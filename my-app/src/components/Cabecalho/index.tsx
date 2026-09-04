b651a0a89f152e19c85263d6f518bec53c22f5b2
Gabriel-Oliveira0611 <gabriel.pereira.0694@outlook.com>
2026-09-03 22:01:37 -0300

    feat : Modularização do app com Cabeçalho, conteudo e rodape.

---
+++ my-app/src/components/Cabecalho/index.tsx
@@ -0,0 +1,8 @@
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>Cabeçalho</h1>
+            <hr />
+        </header>
+    )
+}
---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,15 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2>Conteúdo</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          cupiditate debitis quaerat impedit dolor veniam dicta fugit.
+          Perferendis voluptas eos, unde quidem corporis mollitia quod sequi
+          eius, optio impedit magnam?
+        </p>
+      </section>
+    </main>
+  );
+}
