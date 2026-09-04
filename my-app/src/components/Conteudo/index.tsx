a115a73e4408c3a7869700ff43558d7ff6dba8e2
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-03 22:01:11 -0300

    feat : Modularizando o App com Cabecalho, Conteudo e Rodape.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Conteudo(){
+    return(
+        <main>
+              <section>
+                <h2>Conteúdo</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat dolorem numquam ratione atque! Perspiciatis assumenda magnam neque omnis porro non sequi unde! Eius nihil error delectus et earum id quo.</p>
+            </section>
+        </main>
+    );
+}
