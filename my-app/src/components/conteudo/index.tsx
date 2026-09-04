14a7516746de319681078193ae738fe8016cacdb
eduardo-oliveira-conde <edu.conde021@gmail.com>
2026-09-03 23:02:19 -0300

    criação da pasta componente e criação dos arquivos

---
+++ my-app/src/components/conteudo/index.tsx
@@ -0,0 +1,20 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <section>
+        <figure>
+          <img
+            src="https://placehold.co/300x200/000000/FF0000/png"
+            alt="Figure de demonstração"
+          />
+          <figcaption>Imagem Externa</figcaption>
+        </figure>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
+          dolorem numquam ratione atque! Perspiciatis assumenda magnam neque
+          omnis porro non sequi unde! Eius nihil error delectus et earum id quo.
+        </p>
+      </section>
+    </main>
+  );
+}
